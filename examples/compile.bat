054adfd66590934bf1fa10ae621c601605e9b1b7
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2021-12-03 00:59:09 -0500

    * sort folders

--- examples/compile.bat
+++ examples/compile.bat
@@ -7,6 +7,6 @@ call GOLDbuild -export calc.grm calc.egt %gram%.log
 ::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.c.pgt" %gram%.c
 ::call GOLDprog %gram%.egt PHPEngine.pgt %gram%.php
 ::call GOLDprog grm.cgt ../cgt/JavascriptEngine.pgt grm.js
-call GOLDprog calc.egt ../egt/BaseJSEngine.pgt BaseCalc.js
-call GOLDprog calc.egt ../egt/ImpJSEngine.pgt  Calc.js
+call GOLDprog calc.egt ../templates/egt/BaseJSEngine.pgt BaseCalc.js
+call GOLDprog calc.egt ../templates/egt/ImpJSEngine.pgt  Calc.js
 ::call GOLDprog calc.egt ../egt/TestJSEngine.pgt TestCalc.js
