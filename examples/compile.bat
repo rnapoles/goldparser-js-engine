32cd0d7ffb8cad082f1a240238f5b66593fb9c2d
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2025-07-31 04:42:34 -0500

    Reorder Samples

--- examples/compile.bat
+++ examples/compile.bat
@@ -1,12 +1,12 @@
 cls
 set gram=Simple2
-::call GOLDbuild -export %gram%.grm %gram%.egt %gram%.log
-call GOLDbuild -export calc.grm calc.egt %gram%.log
-::call GOLDprog %gram%.egt ../egt/JavascriptEngine.pgt %gram%.js
-::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.h.pgt" %gram%.h
-::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.c.pgt" %gram%.c
-::call GOLDprog %gram%.egt PHPEngine.pgt %gram%.php
-::call GOLDprog grm.cgt ../cgt/JavascriptEngine.pgt grm.js
-call GOLDprog calc.egt ../templates/egt/BaseJSEngine.pgt BaseCalc.js
-call GOLDprog calc.egt ../templates/egt/ImpJSEngineWithSwitch.pgt  Calc.js
-call GOLDprog calc.egt ../templates/egt/TestJSEngine.pgt TestCalc.js
+::call GOLDbuild -export Calc/%gram%.grm Calc/%gram%.egt Calc/%gram%.log
+call GOLDbuild -export Calc/calc.grm Calc/calc.egt Calc/%gram%.log
+::call GOLDprog %gram%.egt ../egt/JavascriptEngine.pgt Calc/%gram%.js
+::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.h.pgt" Calc/%gram%.h
+::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.c.pgt" Calc/%gram%.c
+::call GOLDprog %gram%.egt PHPEngine.pgt Calc/%gram%.php
+::call GOLDprog grm.cgt ../cgt/JavascriptEngine.pgt Calc/grm.js
+call GOLDprog Calc/calc.egt ../templates/egt/BaseJSEngine.pgt Calc/BaseCalc.js
+call GOLDprog Calc/calc.egt ../templates/egt/ImpJSEngineWithSwitch.pgt Calc/Calc.js
+call GOLDprog Calc/calc.egt ../templates/egt/TestJSEngine.pgt Calc/TestCalc.js
